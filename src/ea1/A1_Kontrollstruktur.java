2699bce7180836c1aad46549cb33488a397991b2
Nico <nicolai.schoenberg@gmail.com>
2014-04-10 04:38:59 -0700

    ea1 aufg 1 und 2 fertig gemacht

--- src/ea1/A1_Kontrollstruktur.java
+++ src/ea1/A1_Kontrollstruktur.java
@@ -3,6 +3,120 @@ package ea1;
 public class A1_Kontrollstruktur {
 
 	public static void main(String[] args) {
-		System.out.print("xx");
+		// Initialisierung
+		int startzahl = 0;
+		int ergebnis = 0;
+
+		try {
+			// Einlesen args[0] in startzahl falls möglich
+			startzahl = Integer.parseInt(args[0]);
+			ergebnis = startzahl;
+			if (ergebnis > 0) {
+				// Wenn die Eingabe größer Null ist, addiere zwei
+				ergebnis = ergebnis + 2;
+			} else {
+				/*
+				 * Wenn die Eingabe kleiner Null ist, mache Eingabe positiv und
+				 * addiere zwei
+				 */
+				ergebnis = ergebnis * (-1) + 2;
+			}
+
+			while (ergebnis > 2) {
+				// Dekrementier Ergebnis solange um zwei, bis es kleiner 2 ist
+				ergebnis = ergebnis - 2;
+			}
+
+			int arbeiten = 4;
+
+			/*
+			 * - Schleife wird 10 mal durchlaufen, extrem schlecht geschrieben -
+			 * Es wird 10*startwert+4 gerechnet - Schleife überflüssig da
+			 * variable arbeiten im weiteren Verlauf nicht mehr verwendet wird
+			 */
+			for (int i = 2; i <= 20; i = i + 2) {
+				arbeiten = arbeiten + startzahl;
+			}
+		} catch (Exception e) {
+			System.out.print("Bitte nur gueltige ganze Zahlen eingeben, ");
+			ergebnis = 3; // Dies führt zu Ausgabe "Fehler!"
+		}
+
+		switch (ergebnis) {
+		case 0:
+			// Ausgabe für ergebnis = 0
+			System.out.println("Das kann nicht sein!");
+			break;
+		case 1:
+			// Ausgabe für ergebnis = 1
+			System.out.println("Die urspruengliche Zahl war ungerade!");
+			break;
+		case 2:
+			// Ausgabe für ergebnis = 2
+			System.out.println("Die urspruengliche Zahl war gerade!");
+			break;
+		default:
+			// Ausgabe für ergebnis < 0 oder > 2
+			System.out.println("Fehler!");
+		}
+		System.out.println("startzahl = " + startzahl);
+	}
+
+	public static void mainx(String[] args) {
+		doit(2);
+	}
+
+	public static void doit(int zahl) {
+		// Initialisierung, einlesen args[0] in startzahl
+
+		// int startzahl = Integer.parseInt(args[0]);
+		int startzahl = zahl;
+		int ergebnis = startzahl;
+
+		if (ergebnis > 0) {
+			// Wenn die Eingabe größer Null ist, addiere zwei
+			ergebnis = ergebnis + 2;
+		} else {
+			/*
+			 * Wenn die Eingabe kleiner Null ist, mache Eingabe positiv und
+			 * addiere zwei
+			 */
+			ergebnis = ergebnis * (-1) + 2;
+		}
+
+		while (ergebnis > 2) {
+			// Dekrementier Ergebnis solange um zwei, bis es kleiner 2 ist
+			ergebnis = ergebnis - 2;
+		}
+
+		int arbeiten = 4;
+
+		/*
+		 * - Schleife wird 10 mal durchlaufen, extrem schlecht geschrieben - Es
+		 * wird 10*startwert+4 gerechnet - Schleife überflüssig da variable
+		 * arbeiten im weiteren Verlauf nicht mehr verwendet wird
+		 */
+		for (int i = 2; i <= 20; i = i + 2) {
+			arbeiten = arbeiten + startzahl;
+		}
+		System.out.println(ergebnis);
+		switch (ergebnis) {
+		case 0:
+			// Ausgabe für ergebnis = 0
+			System.out.println("Das kann nicht sein!");
+			break;
+		case 1:
+			// Ausgabe für ergebnis = 1
+			System.out.println("Die urspruengliche Zahl war ungerade!");
+			break;
+		case 2:
+			// Ausgabe für ergebnis = 2
+			System.out.println("Die urspruengliche Zahl war gerade!");
+			break;
+		default:
+			// Ausgabe für ergebnis < 0 oder > 2
+			System.out.println("Fehler!");
+		}
+		System.out.println("startzahl = " + startzahl);
 	}
 }
--- src/ea1/A2_Fallunterscheidung.java
+++ src/ea1/A2_Fallunterscheidung.java
@@ -2,9 +2,113 @@ package ea1;
 
 public class A2_Fallunterscheidung {
 
+	static final String STRECKE = "Strecke";
+	static final String DREIECK = "Dreieck";
+	static final String RECHTW_DREIECK = "RechtwinkligesDreieck";
+	static final String KREIS = "Kreis";
+	static final String ELLIPSE = "Ellipse";
+	static final String VIERECK = "Viereck";
+	static final String RECHTECK = "Rechteck";
+	static final String FUENFECK = "Fuenfeck";
+	static final String QUADRAT = "Quadrat";
+	static final String SECHSECK = "Sechseck";
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Fallunterscheidung");
+		String geo = args[0];
+
+		if (geo.equals(STRECKE)) {
+			StreckeInfo();
+		} else if (geo.equals(DREIECK)) {
+			DreieckInfo();
+		} else if (geo.equals(RECHTW_DREIECK)) {
+			RechtwDreieckInfo();
+		} else if (geo.equals(KREIS)) {
+			KreisInfo();
+		} else if (geo.equals(ELLIPSE)) {
+			EllipseInfo();
+		} else if (geo.equals(VIERECK)) {
+			ViereckInfo();
+		} else if (geo.equals(RECHTECK)) {
+			RechteckInfo();
+		} else if (geo.equals(FUENFECK)) {
+			FuenfeckInfo();
+		} else if (geo.equals(QUADRAT)) {
+			QuadratInfo();
+		} else if (geo.equals(SECHSECK)) {
+			SechseckInfo();
+		} else {
+			System.out.println("Ich weiß nichts über Ihre eingabe.");
+		}
+	}
+
+	public static void StreckeInfo() {
+		System.out.println("Eine gerade Linie");
+		System.out.println("Kann durch zwei Punkte begrenzt sein");
+		System.out.println("Unendliche Strecken heissen Strahlen");
+		System.out.println("Strecken mit nur einer Begrenzung heissen Halbstrahl");
+	}
+
+	public static void DreieckInfo() {
+		System.out.println("Hat drei Ecken");
+		System.out.println("Auch Triangel genannt");
+		System.out.println("Begrenzungslinien auch Seiten genannt");
+		System.out.println("Pythagoras ... :-)");
+	}
+
+	public static void RechtwDreieckInfo() {
+		System.out.println("Hat drei Ecken");
+		System.out.println("Auch Triangel genannt");
+		System.out.println("Begrenzungslinien auch Seiten genannt");
+		System.out.println("Hat mindestens einen 90 Grad Winkel");
+	}
+
+	public static void KreisInfo() {
+		System.out.println("Rund");
+		System.out.println("Hat einen Mittelpunkt");
+		System.out.println("Flächeninhalt kann näherungsweise mit der Kreiszahl Pi bestimmt werden");
+		System.out.println("Es gibt einen Radius");
+	}
+
+	public static void EllipseInfo() {
+		System.out.println("Geschlossene ovale Kurve");
+		System.out.println("Keine Ecken");
+		System.out.println("Besteht aus zwei Hauptachsen");
+		System.out.println("Hat einen Mittelpunkt");
+	}
+
+	public static void RechteckInfo() {
+		System.out.println("Auch Orthogon genannt");
+		System.out.println("Innenwinkel sind alle rechte Winkel");
+		System.out.println("Gegenüberliegende Seiten sind gleich lang und parallel");
+		System.out.println("Die Winkelsumme beträgt 90°");
+	}
+
+	public static void ViereckInfo() {
+		System.out.println("Oberbegriff fuer viereckige Formen");
+		System.out.println("Hat zwei Diagonalen");
+		System.out.println("Liegen beide Diagonelen innerhalb des Vierecks, so ist es konvex");
+		System.out.println("Liegen beide Diagonelen außerhalb des Vierecks, so ist es nicht konvex");
+	}
+
+	public static void FuenfeckInfo() {
+		System.out.println("Hat fünf Ecken");
+		System.out.println("Ein ebenes Fünfeck besitzt einen eindeutig bestimmbaren Flächeninhalt");
+		System.out.println("Die Summe der Innenwinkel eines regelmäßigen Fünfecks beträgt 540°");
+		System.out.println("Regelmäßiges Fünfeck und Pentagramm bilden eine Grundfigur, in der das Verhältnis des Goldenen Schnittes wiederholt auftritt.");
+	}
+
+	public static void QuadratInfo() {
+		System.out.println("Vier Ecken");
+		System.out.println("Vier Symmetrieachsen");
+		System.out.println("Ist punktsymmetrisch");
+		System.out.println("Ist geschlosse");
+	}
+
+	public static void SechseckInfo() {
+		System.out.println("Sechs ecken");
+		System.out.println("Ist geschlossen");
+		System.out.println("Auch Hexagon genannt");
+		System.out.println("Sind alle sechs Seiten gleich lang, spricht man von einem gleichseitigen Sechseck");
 	}
 
 }
